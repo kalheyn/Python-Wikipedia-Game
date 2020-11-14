3747619f8e1caf05e277e340691941de3e5dd263
Kalman Heyn <kalheyn@gmail.com>
2020-11-13 17:26:40 -0800

    add header

--- python-wikipedia-game.py
+++ python-wikipedia-game.py
@@ -7,6 +7,8 @@ def startGame():
     print(chr(27) + "[2J")
     print("\n~ WELCOME TO THE PYTHON WIKIPEDIA GAME ~\n")
     print("Objective: Navigate from the starting Wikipedia page to the target subject using only the links on each page. Fewest clicks and faster time will score the highest. Press ctrl+C to quit at any time.\n")
+    input("Press ENTER to begin.")
+    print("")
 
 # recursive try/catch for if there are multiple results for a subject. E.g. "United Theological College".
 def checkDisambiguation(subject):
@@ -37,10 +39,28 @@ def setSubjects():
     print("(Start) %s: %s" % (start_subject["subject"].upper(), start_subject["summary"]))
     target_subject = getSubject(target_choice)
     print("(Target) %s: %s" % (target_subject["subject"].upper(), target_subject["summary"]))
-    subjects = {"start": start_subject, "target": target_subject}
-    print("")
+    subjects = {"start": start_subject, "target": target_subject, 'current': start_subject}
+    print("\nThe clock starts when you select your first link. Go!")
     return subjects
+
+def printHeader(player_data):
+    divider_length = 100
+    divider = '=' * divider_length
+    print("")
+    print(divider)
+    print("%s clicks | %s seconds" % (player_data['clicks'], player_data['time']))
+    print("Start: %s" % player_data['start']['subject'].upper())
+    print("Target: %s" % player_data['target']['subject'].upper())
+    print("Current: %s - %s\n" % (player_data['current']['subject'].upper(), player_data['current']['summary']))
+
+def play(player_data):
+    time = 0
+    clicks = 0
+    player_data['time'] = time 
+    player_data['clicks'] = clicks
+    printHeader(player_data)
 
 # Play game
 startGame()
-setSubjects()
+subjects = setSubjects()
+play(subjects)
